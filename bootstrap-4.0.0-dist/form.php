d7fea68ecf8f7a3b936cc9ed3cbfacaaa737cd17
shiikifixer <36285371+shiikifixer@users.noreply.github.com>
2018-02-14 17:13:39 +0900

    Add files via upload

--- bootstrap-4.0.0-dist/form.php
+++ bootstrap-4.0.0-dist/form.php
@@ -7,13 +7,34 @@
 <?php
 print "Hello! Mr. ".$_POST["first_name"]." ".$_POST["last_name"];
 print "Registered your information!";
+$db_host = "mysql:host=webapp-shiiki-mysqldbserver2.mysql.database.azure.com";
+$db_name = "mysqldatabase57520";
+$db_user = "mysqldbuser@webapp-shiiki-mysqldbserver2";
+$db_pass = "elec2EYoh";
+$db_type = "mysql";
+$dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";
+
 try {
-	$pdo = new PDO('mysql:host=webapp-shiiki-mysqldbserver2.mysql.database.azure.com;dbname=mysqldatabase57520;charset=utf8','mysqldbuser@webapp-shiiki-mysqldbserver2','elec2EYoh');
+	$pdo = new PDO($dsn,$db_user,$db__pass);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-	print"successed to connect";
+	print"successed to connect<br>";
 }catch(PDOException $Exception){
-	die('failed to connect:'.$Exception->getMessage());
+	die('failed to connect database : '.$Exception->getMessage());
+}
+
+try {
+	$pdo->beginTransaction();
+	$sql = "INSERT INTO test (last_name, first_name) VALUES ( :last_name, :first_name)";
+	$stmh = $pdo->prepare($sql);
+	$stmh->bindValue(':last_name',$_POST['last_name'],PDO::PARAM STR );
+	$stmh->bindValue(':first_name',$_POST['first_name'],PDO::PARAM STR );
+	$stmh->execute();
+	$pdo->commit();
+	print $stmh->rowCount()." datas are inputed.<br>";
+}catch (PDOException $Exception){
+	$pdo->rollBack();
+	print "failed to insert new data : ".$Exception->getMessage();
 }
 
 ?>
