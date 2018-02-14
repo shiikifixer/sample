ffd79b3f5dfe1b3141c474af54bedfa147298a09
shiikifixer <36285371+shiikifixer@users.noreply.github.com>
2018-02-14 16:18:12 +0900

    Add files via upload

--- bootstrap-4.0.0-dist/form.php
+++ bootstrap-4.0.0-dist/form.php
@@ -5,7 +5,17 @@
 
 <body>
 <?php
-print "Hello! Mr.".$_POST["first_name"].$_POST["last_name"];
+print "Hello! Mr. ".$_POST["first_name"]." ".$_POST["last_name"];
+print "Registered your information!";
+try {
+	$pdo = new PDO('mysql:host=webapp-shiiki-mysqldbserver2.mysql.database.azure.com;dbname=mysqldatabase57520;charset=utf8','mysqldbuser@webapp-shiiki-mysqldbserver2','elec2EYoh');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+	print"successed to connect";
+}catch(PDOException $Exception){
+	die('failed to connect:'.$Exception->getMessage());
+}
+
 ?>
 
 </body>
