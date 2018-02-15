26ef86690c632c8ada206efe0219cb7eb1115d0f
shiikifixer <36285371+shiikifixer@users.noreply.github.com>
2018-02-15 18:02:56 +0900

    Add files via upload

--- bootstrap-4.0.0-dist/form.php
+++ bootstrap-4.0.0-dist/form.php
@@ -16,48 +16,41 @@
 
 	<body class="bg-light">
     <div class="col-md-8 ordere-md-1">
-		<h2> Hello! Mr. <?=$_POST["first_name"];?> <?=$_POST["last_name"];?> <br> </h2>
+		<h3> Hello! Mr. <?=$_POST["first_name"];?> <?=$_POST["last_name"];?> <br> </h3>
+		<h3> python <?=$_POST["exp_python"]?> <br> </h3>
+		<h3> comments <?=$_POST["comments"];?> <br> </h3>
+		
 		</div>
 
 	<?php
-	function reigai(){
-?>
-	<h2> failed to connect database : <br> </h2>
-<?php
-	}
-
-
-
-	$db_host = "wrong-webapp-shiiki-mysqldbserver2.mysql.database.azure.com";
+	$db_host = "webapp-shiiki-mysqldbserver2.mysql.database.azure.com";
 	$db_name = "mysqldatabase57520";
 	$db_user = "mysqldbuser@webapp-shiiki-mysqldbserver2";
 	$db_pass = "elec2EYoh";
 	$db_type = "mysql";
-	$dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";
-
+	$dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";;
+ 
 	try {
 		$pdo = new PDO($dsn,$db_user,$db_pass);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
 		print"successed to connect<br>";
 	}catch(PDOException $Exception){
-		die();
-		reigai();
-?>
-<!--
-		<div class="col-md-8 ordere-md-1">
-		<h2> failed to connect database : <br> </h2>
-		</div>
--->
-	<?php
+		die('failed to connect database : '.$Exception->getMessage());
 	}
 
 	try {
 		$pdo->beginTransaction();
-		$sql = "INSERT INTO test (last_name, first_name) VALUES ( :last_name, :first_name)";
+		$sql = "INSERT INTO test (last_name, first_name, email, address, addres2, country, state, zip) VALUES ( :last_name, :first_name, :email, :address, :address2, :country, :state, :zip)";
 		$stmh = $pdo->prepare($sql);
 		$stmh->bindValue(':last_name',$_POST['last_name'],PDO::PARAM_STR );
 		$stmh->bindValue(':first_name',$_POST['first_name'],PDO::PARAM_STR );
+		$stmh->bindValue(':email',$_POST['email'],PDO::PARAM_STR );
+		$stmh->bindValue(':address',$_POST['address'],PDO::PARAM_STR );
+		$stmh->bindValue(':address2',$_POST['address2'],PDO::PARAM_STR );
+		$stmh->bindValue(':country',$_POST['country'],PDO::PARAM_STR );
+		$stmh->bindValue(':state',$_POST['state'],PDO::PARAM_STR );
+		$stmh->bindValue(':zip',$_POST['zip'],PDO::PARAM_STR );
 		$stmh->execute();
 		$pdo->commit();
 		print $stmh->rowCount()." datas are inserted.<br>";
